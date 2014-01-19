1d8afc8cfce08e95194186c12f8d66cabc2fb82b
Luke Sampson <me@lukesampson.com>
2014-01-19 15:22:51 +1000

    calculate payments for quarter

--- run.py
+++ run.py
@@ -6,28 +6,51 @@ sys.path.append("{}/../refreshbooks".format(dir))
 
 from refreshbooks import api
 from datetime import date, timedelta
+from future import standard_library
 import configparser
-import util
+from util import *
 
 
-start, end = util.last_quarter(date.today())
-print("BAS Quarter: {} - {}".format(util.date_str(start), util.date_str(end - timedelta(days=1))))
+start, end = last_quarter(date.today())
+print("BAS Quarter: {} - {}".format(date_str(start), date_str(end - timedelta(days=1))))
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
 c = api.TokenClient(config.get('auth', 'url'), config.get('auth', 'token'))
 
-res = c.invoice.list(
-	date_from=util.api_date(start),
-	date_to=util.api_date(end)
+print('retrieving payments...')
+res = c.payment.list(
+	date_from=api_date(start),
+	date_to=api_date(end)
 )
 
-pages = int(res.invoices.attrib['pages'])
+pages = int(res.payments.attrib['pages'])
 
 if(pages > 1):
-	raise Exception("there's more than 1 page of invoices! haven't handled this case!")
+	raise Exception("there's more than 1 page of payments! haven't handled this case!")
 
-print('there are {0} pages of invoice'.format(pages))
+gross = 0
+taxable = 0
+gst = 0
 
-# print(objectify.dump(res))
+for payment in res.payments.payment:
+	invoice_id = payment.invoice_id.pyval
+	invoice_res = c.invoice.get(invoice_id=invoice_id)
+	invoice = invoice_res.invoice
+
+	invoice_taxable, invoice_taxes = tax_amounts(invoice)
+	fraction_paid = payment.amount.pyval / invoice.amount.pyval
+
+	date_paid = parse_api_date(payment.date)
+	print("received payment of ${} ({:.2%}) of invoice #{} (${}) on {}".format(payment.amount,
+		fraction_paid, invoice.number, invoice.amount, date_paid))
+
+	payment_taxable = invoice_taxable * fraction_paid
+	payment_gst = invoice_taxes['GST'] * fraction_paid
+
+	gross += payment.amount.pyval
+	taxable += payment_taxable
+	gst += payment_gst
+
+print("collected: gross=${:.2f}, taxable=${:.2f}, GST=${:.2f}".format(gross, taxable, gst))
--- util.py
+++ util.py
@@ -1,4 +1,7 @@
-from datetime import date
+from datetime import date, datetime
+from decimal import *
+
+API_DATE_FORMAT = '%Y-%m-%d 00:00:00'
 
 def last_quarter(before_day):
 	q_months = [1, 4, 7, 10] # months that start a quarter
@@ -19,4 +22,24 @@ def date_str(date):
 	return date.strftime('%-d %b, %Y')
 
 def api_date(date):
-	return date.strftime('%Y-%m-%d 00:00:00')
+	return date.strftime(API_DATE_FORMAT)
+
+def parse_api_date(str_el):
+	return datetime.strptime(str_el.pyval, API_DATE_FORMAT).date()
+
+def tax_amounts(invoice):
+	taxable = 0
+	taxes = {}
+
+	for line in invoice.lines.line:
+		if(line.tax1_name):
+			taxable = taxable + line.amount.pyval
+			add_tax(taxes, line.tax1_name, line.tax1_percent, line.amount.pyval)
+		elif(line.tax2_name):
+			taxable += line.amount.pyval
+			add_tax(taxes, line.tax1_name, line.tax1_percent, line.amount.pyval)
+
+	return taxable, taxes
+
+def add_tax(taxes, name, percent, line_amount):
+	taxes[name] = taxes.get(name, Decimal(0)) + percent / Decimal(100) * line_amount
